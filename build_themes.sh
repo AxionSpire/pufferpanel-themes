f2e2e741879da6368b1084ab798bd1c37f5a7ee6
aelithron <aelithron@gmail.com>
2024-11-20 18:11:48 -0700

    Build and theme fixes Fixes to the building process. Also, more things with Lavender theme!

--- build_themes.sh
+++ build_themes.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
 mkdir -p output
+echo "Building themes..."
 for dir in */; do
     # Skip unwanted folders
     [ "$dir" = ".github/" ] && continue
@@ -10,10 +11,14 @@ for dir in */; do
     theme_name=$(basename "$dir")
 
     # Check required files exist
-    if [[ -f "$dir/theme.json" ]]; then
-        find "$dir" -name "*.png" -o -name "*.jpg" | tar -cf "output/${theme_name}.tar" -C "$dir" theme.css theme.json -T -
+    if [[ -f "$dir/theme.json" && -f "$dir/theme.css" ]]; then
+        if [[ -d "$dir/img" ]]; then
+            tar -cf "output/${theme_name}.tar" -C "$dir" theme.css theme.json img/
+        else
+            tar -cf "output/${theme_name}.tar" -C "$dir" theme.css theme.json
+        fi
         echo "Built $theme_name successfully."
     else
-        echo "Skipping $theme_name as a required file is missing."
+        echo "Skipping $theme_name as one or more required files are missing."
         fi
     done
